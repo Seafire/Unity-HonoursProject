8179c190528b57ba412d81444bfcc82a90c282a4
Conor <ConorP95@hotmail.co.uk>
2016-11-30 22:49:24 +0000

    Added in core functions to select and deselect object Added project to visualize the current unit being selected

--- HonoursProjectUnity/Assets/Scipts/MousePoint.cs
+++ HonoursProjectUnity/Assets/Scipts/MousePoint.cs
@@ -6,7 +6,15 @@ public class MousePoint : MonoBehaviour
 {
 	RaycastHit hit;
 
+	public static GameObject currentlySelectedUnit;
+	private Vector3 mouseDownPoint;
+
 	public GameObject target;
+
+	void Awake()
+	{
+		mouseDownPoint = Vector3.zero;
+	}
 
 	// Use this for initialization
 	void Start () 
@@ -21,6 +29,12 @@ public class MousePoint : MonoBehaviour
 
 		if (Physics.Raycast(ray, out hit, 1000))
 		{
+
+			// Store point of mouse click
+			if (Input.GetMouseButtonDown(0))
+			{
+				mouseDownPoint = hit.point;
+			}
 			Debug.Log (hit.collider.name);
 			if (hit.collider.name == "Plane")
 			{
@@ -33,4 +47,29 @@ public class MousePoint : MonoBehaviour
 		}
 		Debug.DrawRay (ray.origin, ray.direction * 1000 , Color.red);
 	}
+
+	public bool DidUserLeftClick(Vector3 hitPoint)
+	{
+		float clickZone = 1.3f;
+
+		if (
+			(mouseDownPoint.x < hitPoint.x + clickZone && mouseDownPoint.x > hitPoint.x - clickZone) &&
+			(mouseDownPoint.y < hitPoint.y + clickZone && mouseDownPoint.y > hitPoint.y - clickZone) &&
+			(mouseDownPoint.z < hitPoint.z + clickZone && mouseDownPoint.z > hitPoint.z - clickZone)
+			)
+			return true;
+		else
+			return false;
+
+	}
+
+	public static void DeselectGameObject()
+	{
+		if (currentlySelectedUnit != null)
+		{
+			currentlySelectedUnit.transform.FindChild("Selected").gameObject.SetActive(false);
+			currentlySelectedUnit = null;
+		}
+	}
+
 }
