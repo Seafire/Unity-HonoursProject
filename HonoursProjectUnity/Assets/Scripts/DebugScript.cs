a28a378372e7968d949c6796cbc1a840919e5bd5
Conor <ConorP95@hotmail.co.uk>
2017-02-15 13:29:41 +0000

    Created Debug script to test mechanics

---
+++ HonoursProjectUnity/Assets/Scripts/DebugScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugScript : MonoBehaviour 
+{
+	GameObject enemy;
+	EnemyAI enemyAI;
+
+	// Use this for initialization
+	void Start () 
+	{
+		enemy = GameObject.Find ("Unit_Enemy (2)");
+		enemyAI = enemy.GetComponent<EnemyAI> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey (KeyCode.Space))
+		{
+			Debug.Log ("Run");
+			enemyAI.charStats.morale = 10;
+			enemyAI.stateAI = EnemyAI.StateAI.retreat;
+			enemyAI.plControl.runSpeed = 3.0f;
+		}
+	}
+}
